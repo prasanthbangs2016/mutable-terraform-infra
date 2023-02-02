95f9b8e29dff52ad5e293ac1009f6cb8f63a4aa6
prasanthbangs2016 <prasanthbangs@gmail.com>
2023-02-03 01:28:30 +0530

    added tf file

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
 output "app_subnets" {
   value = local.apps_subnet_ids
 }
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "WORKSTATION_IP" {
+  value = var.WORKSTATION_IP
+}
+
+output "vpc_cidr" {
+  value = var.vpc_cidr_block
+}
